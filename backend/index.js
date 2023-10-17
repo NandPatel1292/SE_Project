df187a3586fcbee627c6f533e01dfe065944dd5f
Yash Chotaliya <yashchotaliya@Yashs-MacBook-Air.local>
2023-10-17 15:00:18 +0530

    y: error handler added

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,9 @@ const dotenv = require("dotenv");
 
 // all variables
 const app = express();
+const port = process.env.PORT || 3000;
+
+console.log(process.env.PORT);
 
 // config
 dotenv.config();
@@ -15,10 +18,16 @@ app.get("/", function (req, res) {
 
 // error handeling
 app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
 
+  res.status(statusCode).json({
+    success: false,
+    message,
+  });
 })
 
 // Server connect
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log("Example app listening on port 3000!");
 });
